2d75e02816724803340f5d55d8bf472cd999f1fa
Harshal Margaj <harshal.margaj12@gmail.com>
2025-12-23 18:57:04 +0530

    feat: implement routing and add initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import MobileLayout from "./components/MobileLayout";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
-	return <MobileLayout>MobileLayout</MobileLayout>;
+	return (
+		<BrowserRouter>
+			<MobileLayout>
+				<Routes>
+					<Route path="/" />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/profile" element={<Profile />} />
+				</Routes>
+			</MobileLayout>
+		</BrowserRouter>
+	);
 };
 
 export default App;
